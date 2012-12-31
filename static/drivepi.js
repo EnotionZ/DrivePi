04a7a1d44508025bd28811dcefbe285b0db27f0a
Dominick Pham <dominick.pham@gmail.com>
2012-12-31 13:38:47 -0500

    organizing code

--- static/drivepi.js
+++ static/drivepi.js
@@ -1,34 +1,63 @@
 (function() {
+	var debug = true, mediaStream;
+	var DrivePi = window.DrivePi = {};
 
-	window.DrivePi = {};
-	var debug = true;
-	var mediaStream;
+	/**
+	 * Setting up WebSocket handler
+	 */
+	var ws = DrivePi.ws = new WebSocket('WS://' + location.host + '/websocket');
+	var changeHandler = function() { ws.send(this.name + ' ' + this.value); };
+	var mouseupHandler = function() { this.value = 0; changeHandler.call(this); };
+	if(debug) ws.addEventListener('message', function(e) { console.log(e.data); });
+	ws.addEventListener('open',  function() {
+		turnRng.addEventListener('change', changeHandler);
+		turnRng.addEventListener('mouseup', mouseupHandler);
+		driveRng.addEventListener('change', changeHandler);
+		driveRng.addEventListener('mouseup', mouseupHandler);
 
-	var startVideo = function() {
+		if(debug) ws.send('Connected ' + (new Date()));
+	});
+
+
+	/**
+	 * Load WebRTC stream
+	 */
+	ws.addEventListener('open', function() {
 		navigator.getUserMedia = navigator.getUserMedia ||
 			navigator.webkitGetUserMedia ||
 			navigator.mozGetUserMedia ||
 			navigator.msGetUserMedia;
+
 		if(typeof navigator.getUserMedia !== 'function') return;
 		navigator.getUserMedia({video: true, audio: false}, function(localMediaStream){
 			mediaStream = localMediaStream;
 			webrtcVideo.src = (window.URL || window.webkitURL).createObjectURL(localMediaStream);
 		});
 
-		webrtcVideo.addEventListener('loadeddata', function() {
-			var videoRatio = webrtcVideo.videoHeight/webrtcVideo.videoWidth;
-			var videoWidth = 300, videoHeight = videoWidth*videoRatio;
-			var interval = 10, videoTimer;
-			canvas.width = videoWidth;
-			canvas.height = videoHeight;
-			var ctx = canvas.getContext('2d');
-			ctx.strokeStyle = '#ff0000';
-			var initialArea;
+	});
 
-			var processVideo = function() {
+
+	/**
+	 * Face detection
+	 */
+	webrtcVideo.addEventListener('loadeddata', function() {
+		var videoRatio = webrtcVideo.videoHeight/webrtcVideo.videoWidth;
+		var videoWidth = 300, videoHeight = videoWidth*videoRatio;
+		var interval = 10, videoTimer, ctx, initialArea;
+
+		canvas.width = videoWidth;
+		canvas.height = videoHeight;
+		ctx = canvas.getContext('2d');
+		ctx.strokeStyle = '#ff0000';
+
+		DrivePi.pause = function() { if(videoTimer) clearInterval(videoTimer); };
+		DrivePi.play = function() {
+			DrivePi.pause();
+			videoTimer = setInterval(function() {
+				if(!mediaStream) return false;
 				ctx.drawImage(webrtcVideo, 0, 0, videoWidth, videoHeight);
 				var c = ccv.detect_objects({
-					canvas : (canvas),
+					canvas : canvas,
 					cascade : cascade,
 					interval : interval,
 					min_neighbors : 1
@@ -39,37 +68,15 @@
 
 					var normalizedTurnValue = (c[0].x-150)/10;
 					turnRng.value = normalizedTurnValue;
+					changeHandler.call(turnRng);
 					//driveRng.value = (c[0].width*c[0].height-initialArea)/initialArea;
 				}
-			};
-			DrivePi.pause = function() { if(videoTimer) clearInterval(videoTimer); };
-			DrivePi.play = function() {
-				DrivePi.pause();
-				videoTimer = setInterval(function() {
-					if(mediaStream) processVideo();
-				}, interval);
-			};
+			}, interval);
+		};
 
-			DrivePi.play();
-			btnPlay.addEventListener('click', DrivePi.play);
-			btnPause.addEventListener('click', DrivePi.pause);
-		});
-	};
+		DrivePi.play();
 
-	var ws = new WebSocket('WS://' + location.host + '/websocket');
-	ws.onopen = function() {
-
-		var changeHandler = function() { ws.send(this.name + ' ' + this.value); };
-		var mouseupHandler = function() { this.value = 0; changeHandler.call(this); };
-
-		turnRng.addEventListener('change', changeHandler);
-		turnRng.addEventListener('mouseup', mouseupHandler);
-		driveRng.addEventListener('change', changeHandler);
-		driveRng.addEventListener('mouseup', mouseupHandler);
-
-		if(debug) ws.send('Connected ' + (new Date()));
-
-		startVideo();
-	};
-	if(debug) ws.addEventListener('message', function(e) { console.log(e.data); });
+		btnPlay.addEventListener('click', DrivePi.play);
+		btnPause.addEventListener('click', DrivePi.pause);
+	});
 })();
